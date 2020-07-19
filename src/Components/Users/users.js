a3fee6f7803a8370b466fd7ce1e7f9a32a0a826a
albert.tarkaa <albert.tarkaa@tenece.com>
2020-07-19 23:55:44 +0100

    final commit for v1

--- src/Components/Users/users.js
+++ src/Components/Users/users.js
@@ -9,7 +9,7 @@ const Users = () => {
   return (
     <div style={userStyle}>
       {loading
-        ? 'Loading...'
+        ? ''
         : users.map((user) => <UserItem key={user.id} user={user} />)}
     </div>
   );
